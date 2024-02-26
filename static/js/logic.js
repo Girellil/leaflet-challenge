f4676d863c0277a0ee7dea2b5da48bfdbd1a49f6
Girellil <64373104+Girellil@users.noreply.github.com>
2024-02-26 16:27:17 -0500

    Challenge solution
    JS Logic Comments.

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,21 +1,24 @@
-// Create our map, giving it the streetmap and earthquakes layers to display on load.
+// Create map, broad zoom onto North/Central America.
 let myMap = L.map("map", {
     center: [35, -100],
     zoom: 4
 });
 
+// Add Layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-// Use this link to get the GeoJSON data.
+// Create link to request data.
 let link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Getting our GeoJSON data
+// Getting data and start an. function to set the data to the map
 d3.json(link).then(function(data) {
     
+    // Get Features into a new variable
     let features = data.features;
 
+    // Create Legend Limits and labels, along with Red values added to RGB format.
     let colorBins = [2.5, 5, 10, 50, 90];
     let redScales = [255, 204, 153, 102, 51, 0];
     let colorLabels = [`< ${colorBins[0]}`,
@@ -31,6 +34,7 @@ d3.json(link).then(function(data) {
         colorScales.push('rgb(' + redScales[i] + ', 0, 0)')
     }
 
+    // Create Function to determine Color of each circle (to return R value of RGB)
     function getCircleColor(eDepth){
         let redScale;
         if(eDepth < colorBins[0]){
@@ -52,19 +56,22 @@ d3.json(link).then(function(data) {
     // Loop through each feature
     features.forEach(function(feature) {
 
-        // Get Magnitude date and place of each earthquake
+        // Get place time and magnitud of the earthquake
         let place = feature.properties.place;
         let time = Date(feature.properties.time);
         let mag = feature.properties.mag;
         
-        // Retrieve the "coordinates" array within the geometry object
+        // Retrieve the coordinates from the geometry of the earthquake
         let geometry = feature.geometry;
         let latitude = geometry.coordinates[1];
         let longitude = geometry.coordinates[0];
         let depth = geometry.coordinates[2];
 
+        // Call get color to set fillcolor based on Depth
+        // Colors are tones of RED input as RGB on the function below
         let red = getCircleColor(depth);
 
+        // Create Circles + Bind POP-UP with relevant info of the EQ.
         L.circle([latitude, longitude], {
             color: 'rgb(' + red + ', 0, 0)',
             fillColor: 'rgb(' + red + ', 0, 0)',
@@ -75,7 +82,7 @@ d3.json(link).then(function(data) {
 
     });
 
-    // Set up the legend.
+    // Setup legend.
     let legend = L.control({ position: "bottomright" });
     legend.onAdd = function () {
         let div = L.DomUtil.create('div', 'info legend'),
@@ -83,16 +90,17 @@ d3.json(link).then(function(data) {
         colors = colorScales,
         labels = [];
 
-    // Add title to the legend
+    // Legend title
     div.innerHTML = '<h1>Earthquake Depth</h1>'
     
-    // Loop through our density intervals and generate a label with a colored square for each interval
+    // Loop through lables intervals to generate colored square for each + Label Name
     for (let i = 0; i < grades.length; i++) {
         // Add rectangle filled with respective color
         div.innerHTML +=
             '<span style="background-color:' + colors[i] + ';width:35px;height:25px;display:inline-block;margin-right:5px;"></span>' +
             grades[i] + '<br>';
     }
+    //Formatting legend with a white Background and rounding corners
     div.style.backgroundColor = "white";
     div.style.borderRadius = "10px";
     return div;
